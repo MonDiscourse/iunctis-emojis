d07de9b30a01f278e7ed9522a5286e0d3e19f197
iunctis <steven.abgrall@gmail.com>
2016-12-02 01:01:45 +0100

    Tristesse
    Ajout emojis tristesse

---
+++ plugin.rb
@@ -0,0 +1,27 @@
+# name: iunctis-emojis
+# about: iunctis emojis
+# version: 1.0
+# authors: Steven et Tyler (pas le chanteur)
+# url: https://github.com/iunctis/iunctis-emojis
+
+enabled_site_setting :emoji_enabled
+
+register_emoji "blam", "/plugins/iunctis-emojis/images/tristesse/blam.gif"
+register_emoji "calme", "/plugins/iunctis-emojis/images/tristesse/calme.gif"
+register_emoji "chirac", "/plugins/iunctis-emojis/images/tristesse/chirac.png"
+register_emoji "emocry", "/plugins/iunctis-emojis/images/tristesse/emocry.gif"
+register_emoji "jlow", "/plugins/iunctis-emojis/images/tristesse/jlow.png"
+register_emoji "jordray", "/plugins/iunctis-emojis/images/tristesse/jordray.png"
+register_emoji "jorjor", "/plugins/iunctis-emojis/images/tristesse/jorjor.png"
+register_emoji "jorpass", "/plugins/iunctis-emojis/images/tristesse/jorpass.png"
+register_emoji "jorpop", "/plugins/iunctis-emojis/images/tristesse/jorpop.png"
+register_emoji "jorwest", "/plugins/iunctis-emojis/images/tristesse/jorwest.png"
+register_emoji "kbell", "/plugins/iunctis-emojis/images/tristesse/kbell.gif"
+register_emoji "kidouin", "/plugins/iunctis-emojis/images/tristesse/kidouin.png"
+register_emoji "lebron2", "/plugins/iunctis-emojis/images/tristesse/lebron2.png"
+register_emoji "lebron4", "/plugins/iunctis-emojis/images/tristesse/lebron4.png"
+register_emoji "moquette", "/plugins/iunctis-emojis/images/tristesse/moquette.gif"
+register_emoji "mott", "/plugins/iunctis-emojis/images/tristesse/mott.gif"
+register_emoji "romo", "/plugins/iunctis-emojis/images/tristesse/romo.png"
+register_emoji "sadboy", "/plugins/iunctis-emojis/images/tristesse/sadboy.gif"
+register_emoji "sarko", "/plugins/iunctis-emojis/images/tristesse/sarko.png"
